56e1de12183506ac8f94d3004fba666b4eb07baf
AndikaDwiSaputra <94271807+AndikaDwiSaputra@users.noreply.github.com>
2021-11-14 11:28:08 +0700

    Update uncrush.py

--- uncrush.py
+++ uncrush.py
@@ -1,5 +1,10 @@
 nama ='Andika Dwi Saputra'
 umur =18
 asal ='Jember'
-
-print(nama)
+kuliah = 'Universitas Jember'
+prodi = 'Teknologi Informasi'
+hobi = 'bermain game'
+print(
+    'Hai gaess,perkenalkan nama saya', nama,', umur saya', umur,', 
+    'saya berasal dari', asal,', saya kuliah di', kuliah,',
+    'program studi', prodi,', hobi saya', hobi,', oke salken ya gaessss:)
