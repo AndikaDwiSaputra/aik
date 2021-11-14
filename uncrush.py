3b5de5b733eff0eb63fa033ddebaa8f414994466
AndikaDwiSaputra <94271807+AndikaDwiSaputra@users.noreply.github.com>
2021-11-14 11:48:25 +0700

    Update uncrush.py

--- uncrush.py
+++ uncrush.py
@@ -4,8 +4,4 @@ asal ='Jember'
 kuliah = 'Universitas Jember'
 prodi = 'Teknologi Informasi'
 hobi = 'bermain game'
-print(
-    'Hai gaess,perkenalkan nama saya', nama,', umur saya', umur,', 
-    'saya berasal dari', asal,', saya kuliah di', kuliah,',
-    'program studi', prodi,', hobi saya', hobi,', oke salken ya gaessss
-)
+print('Hai gaess,perkenalkan nama saya', nama,', umur saya', umur,'saya berasal dari', asal,', saya kuliah di', kuliah,'program studi', prodi,', hobi saya', hobi,', oke salken ya gaessss)
